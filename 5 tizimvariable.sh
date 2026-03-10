7aaa76ca61ef2b2acc2806e0c6fdf90dbe057b27
bobur0777 <boburabdumalikov777@gmail.com>
2026-03-10 17:26:08 +0500

    Bash script amaliy vazifa: 5-topshiriq yuklandi

---
+++ 5 tizimvariable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Skript nomi: $0"              # Skriptning fayl nomi
+echo "Hozirgi foydalanuvchi: $xurshida" # Tizimga kirgan user
+echo "Hozirgi papka: $PWD"          # Ishchi katalog
+echo "Skriptning PID raqami: $$"    # Process ID
+
